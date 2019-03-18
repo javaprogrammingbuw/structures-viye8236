e33d32659926e82f72d585f9e7dc629f7c5e974d
viye8236 <33550570+viye8236@users.noreply.github.com>
2019-03-18 22:55:45 +0100

    Add files via upload
    i couldn't commit with git bash

---
+++ learnjava/structures-viye8236/EvenOdd.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		//todo: The program takes a number as input.
+		//It then goes through all integer values till reaching that value and prints all the odd ones.
+		System.out.println("Enter the values");
+		int a = scan.nextInt();
+		//loops
+		for (int i=0; i<a; i++) {
+			if (i%2 != 0) {
+				System.out.println(i+"");
+			}
+		}
+		
+	}
+	
+}
+
---
+++ learnjava/structures-viye8236/SumMeanMinMax.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class SumMeanMinMax {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		System.out.println(" Enter the value");
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		
+		
+		
+		double s = a+b+c;
+		System.out.println("Sum of the values" +s);
+		
+		// mean
+		
+		double average = s/3;
+				System.out.println(" mean of the values " +average);
+				
+				
+				if (a>b && a>c) {
+					
+					System.out.println("Maximum value is A" +a);
+				}
+				else if(b>c && b>a){
+
+					System.out.println("Maximum value is B" +b);
+				}else {
+					System.out.println("Maximum value is C" + c);
+				}
+				if (a<b && a<c) {
+					System.out.println("Minimum value is A" + a );
+				}else if(b<c && b<a) {
+					System.out.println("Minimum value is B "+ b);
+				}else {
+					System.out.println("Minimum value is C "+ c);
+				}
+		/*
+		 * Todo: Print the sum, mean, max Value and min Value of a, b and c.
+		 * */
+		
+	}
+
+}
